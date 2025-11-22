be81ae292f24a26854796f37dc713c54347ef7f4
GuidoMinchella <guidominchella3@gmail.com>
2025-11-22 13:28:41 +0100

    Update Header.tsx

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -48,7 +48,7 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-mediterranean-bianco shadow-lg sticky top-0 z-50">
+    <header className="bg-mediterranean-bianco shadow-lg fixed top-0 left-0 right-0 z-50">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
